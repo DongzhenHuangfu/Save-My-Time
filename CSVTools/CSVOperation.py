7daf37f1365ef24fbdb92a9a97bb93eef1281453
DongzhenHuangfu <wyatt_h@outlook.com>
2020-02-08 04:02:37 +0100

    feat: define EndRow and EndColumn
    define variables for end row number and end column number

--- CSVTools/CSVOperation.py
+++ CSVTools/CSVOperation.py
@@ -1,6 +1,6 @@
 import csv
 
-def transform(ReadName, StartRow, StartColumn, SaveName = ""):
+def transform(ReadName, StartRow, StartColumn, EndRow = -1, EndColumn = -1, SaveName = ""):
 	if SaveName == "":
 		SaveName = ReadName + "_transform.csv"
 	else:
@@ -13,12 +13,16 @@ def transform(ReadName, StartRow, StartColumn, SaveName = ""):
 
 		for row in CSVFile:
 			count += 1
-			if count < StartRow:
+			if count < StartRow or (count > EndRow and EndRow != -1):
 				Ret.append(row)
 				continue
 			Ret.append([])
 
 			for j in range(len(row)):
+				if j + 1 > EndColumn and EndColumn != -1:
+					Ret.append(["" for i in range(StartColumn-1)])
+					Ret[-1] += row[j:-1]
+					break
 				if j + 1 <= StartColumn:
 					Ret[-1].append(row[j])
 				else:
